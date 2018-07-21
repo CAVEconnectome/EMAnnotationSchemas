ac420409c1560c0f0845ac41e365fd6370c1349a
Forrest Collman <forrest.collman@gmail.com>
2018-07-21 08:16:03 -0700

    removing comments

--- emannotationschemas/synapse.py
+++ emannotationschemas/synapse.py
@@ -2,7 +2,6 @@ from emannotationschemas.base import BoundSpatialPoint, \
     SpatialPoint, \
     AnnotationSchema
 import marshmallow as mm
-# from marshmallow import validate
 
 
 class SynapseSchema(AnnotationSchema):
@@ -20,20 +19,3 @@ class SynapseSchema(AnnotationSchema):
     @mm.post_load
     def validate_type(self, item):
         assert item['type'] == 'synapse'
-
-
-# class BulkSynapse(mm.Schema):
-#     pts = mm.fields.List(mm.fields.Float,
-#                          required=True,
-#                          validate=validate.Length(equal=9),
-#                          description="pre(x,y,z) center(x,y,z) post(x,y,z)"
-#                                      "position of synapse")
-
-#     @mm.post_load
-#     def unwrap_data(self, data):
-
-#         pts = item.pop('pts')
-#         item['pre_pt']={'position':pts[0:3]}
-#         item['center_pt']={'position':pts[3:6]}
-#         item['post_pt']={'position':pts[6:9]}
-#         item['type']='synapse'
