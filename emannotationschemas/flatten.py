7ad2f2e436ac9b225d184c731b0cbf2d8463187b
Forrest Collman <forrest.collman@gmail.com>
2018-07-21 11:23:27 -0700

    adding codecov and flake8

--- emannotationschemas/flatten.py
+++ emannotationschemas/flatten.py
@@ -1,13 +1,6 @@
 import marshmallow as mm
 
 
-class FlatSchema(mm.Schema):
-
-    def __init__(self, additional_fields=None, **kwargs):
-        super().__init__(**kwargs)
-        self.declared_fields.update(additional_fields)
-
-
 def flatten_fields(schema, root=None, sep="_"):
     fields = {}
     if root is None:
@@ -23,7 +16,7 @@ def flatten_fields(schema, root=None, sep="_"):
 
 
 def create_flattened_schema(BaseSchema, sep="_"):
-    new_fields = flatten_fields(BaseSchema, sep)
+    new_fields = flatten_fields(BaseSchema, sep=sep)
     FlatSchema = type('Flat{}'.format(BaseSchema.__name__),
                       (mm.Schema,),
                       new_fields)
