8dfcf40b9fe472200d4418be61541a04fa071af0
Forrest Collman <forrest.collman@gmail.com>
2018-07-31 18:57:16 -0400

    adding documentation

--- emannotationschemas/synapse.py
+++ emannotationschemas/synapse.py
@@ -18,12 +18,20 @@ class SynapseSchema(AnnotationSchema):
 
     @mm.post_load
     def validate_type(self, item):
+        # check that the annotation type is present in the object as 'synapse'
         assert item['type'] == 'synapse'
+
         pre_id = item['pre_pt'].get('root_id', None)
         post_id = item['post_pt'].get('root_id', None)
+
+        # if the root_id is present we should set the valid flag depending up on this rule
+        # when the root_id is not present (i.e. when posting new annotations with no root_id's in mind)
+        # then the valid flag should be not present
         if pre_id is not None:
             if (pre_id == post_id):
                 item['valid'] = False
             else:
                 item['valid'] = True
+        else:
+            item.pop('valid', None)
         return item
