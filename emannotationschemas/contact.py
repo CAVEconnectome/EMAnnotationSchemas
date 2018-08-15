9e18ac49839d89cbfcbdbf0eefb551001ff34fb2
Forrest Collman <forrest.collman@gmail.com>
2018-08-15 10:02:54 -0700

    changed to BoundSpatialPoints

--- emannotationschemas/contact.py
+++ emannotationschemas/contact.py
@@ -5,30 +5,19 @@ import marshmallow as mm
 
 class Contact(AnnotationSchema):
     area = mm.fields.Float(description="contact area")
-    sidea_pt = mm.fields.List(mm.fields.Int,
-                              validate=mm.validate.Length(equal=3),
+    sidea_pt = mm.fields.Nested(BoundSpatialPoint,
                               required=True,
-                              description='spatial position in voxels of'
-                                          'x,y,z of side a of contact',
-                              postgis_geometry='POINTZ')
-    sideb_pt = mm.fields.List(mm.fields.Int,
-                              validate=mm.validate.Length(equal=3),
+                              description='point on sidea of contact')
+    sideb_pt = mm.fields.Nested(BoundSpatialPoint,
                               required=True,
-                              description='spatial position in voxels of'
-                                          'x,y,z of side b of contact',
-                              postgis_geometry='POINTZ')
-    ctr_pt = mm.fields.List(mm.fields.Int,
-                              validate=mm.validate.Length(equal=3),
-                              required=True,
-                              description='spatial position in voxels of'
-                                          'x,y,z of contact',
-                              postgis_geometry='POINTZ')ad
+                              description='point on sideb of contact')
+    ctr_pt = mm.fields.Nested(SpatialPoint,
+                              description='point on contact interface')
     sidea_supervoxel_ids = mm.fields.List(mm.fields.Int,
                                           description = "list of supervoxel ids on sidea")
     sideb_supervoxel_ids = mm.fields.List(mm.fields.List,
                                           description ="list of superevoxel ids on sideb")
-
-
+                                          
     @mm.post_load
     def validate_type(self, item):
         # check that the annotation type is present in the object as 'synapse'
