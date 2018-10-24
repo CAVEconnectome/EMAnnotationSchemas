0af6ea15e6558556ee371e081c65a516788174a0
fcollman <forrest.collman@gmail.com>
2018-10-23 17:33:00 -0700

    Bump version: 0.3.4 → 0.3.5

--- emannotationschemas/__init__.py
+++ emannotationschemas/__init__.py
@@ -6,7 +6,7 @@ from emannotationschemas.functional_coregistration import FunctionalCoregistrati
 from emannotationschemas.postsynaptic_compartment import PostsynapticCompartment
 from emannotationschemas.cell_type_local import CellTypeLocal
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 type_mapping = {
     'synapse': SynapseSchema,
