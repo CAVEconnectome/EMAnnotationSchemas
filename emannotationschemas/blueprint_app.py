d1141302abf00b764f90e4a1b103efc44c76f604
Forrest Collman <forrest.collman@gmail.com>
2019-03-11 11:19:27 -0700

    adding a human readable view to schema service

--- emannotationschemas/blueprint_app.py
+++ emannotationschemas/blueprint_app.py
@@ -36,3 +36,25 @@ def get_type_schema(annotation_type):
 @bp.route("/type/<annotation_type>")
 def get_type_schema_route(annotation_type):
     return jsonify(get_type_schema(annotation_type))
+
+
+@bp.route("/type/<annotation_type>/view")
+def get_schema_view(annotation_type):
+    Schema = get_schema(annotation_type)
+    print(dir(Schema))
+    ds = []
+    for col, field in Schema._declared_fields.items():
+        if isinstance(field, mm.fields.Nested):
+            print(dir(field.schema), field.schema)
+            schema = field.schema.__class__.__name__
+        else:
+            schema = ''
+
+        ds. append({
+            'field_name': col,
+            'description': field.metadata.get('description', ''),
+            'type': type(field).__name__,
+            'schema': schema
+        })
+    df = pd.DataFrame(ds)
+    return df[['field_name', 'type', 'description', 'schema']].to_html()
