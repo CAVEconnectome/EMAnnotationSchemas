05054a71c8f44d1e3b7fd04d2d315103f45bc5ad
Forrest Collman <forrest.collman@gmail.com>
2019-08-14 09:38:14 -0700

    add make_annotation_flask_schema

---
+++ emannotationschemas/sqlalchemy_schemas.py
@@ -0,0 +1,14 @@
+from flask_marshmallow import Marshmallow
+
+ma = Marshmallow()
+
+def make_annotation_flask_schema(model):
+    model_name = model.__name__.capitalize() + "Schema"
+    meta_attrd = {
+        "model": model
+    }
+    metaclass = type("Meta", (), meta_attrd)
+    attrd = {
+        'Meta:': metaclass,
+    }
+    return type(model_name, (ma.ModelSchema,), attrd)
