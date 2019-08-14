9f738376ee6722001dc5bdf172c913bd8ecd03b4
Forrest Collman <forrest.collman@gmail.com>
2019-08-14 13:36:07 -0700

    fixing NumericField

--- emannotationschemas/sqlalchemy_schemas.py
+++ emannotationschemas/sqlalchemy_schemas.py
@@ -4,7 +4,7 @@ import geoalchemy2
 from shapely import geometry
 from geoalchemy2.shape import from_shape, to_shape
 from marshmallow import fields, pre_load, post_dump, ValidationError
-
+import sqlalchemy
 
 ma = Marshmallow()
 
@@ -23,9 +23,29 @@ class GeometryField(fields.Field):
     def _deserialize(self, value, attr, data):
         if value is None:
             return None
+        return int(value)
+
+class NumericField(fields.Field):
+    """
+    Use shapely and geoalchemy2 to serialize / deserialize a point
+    Does make a big assumption about the data being spat back out as
+    JSON, but what the hey.
+    """
+
+    def _serialize(self, value, attr, obj):
+        if value is None:
+            return None
+        return int(value)
+
+    def _deserialize(self, value, attr, data):
+        if value is None:
+            return None
         return from_shape(geometry.shape(value))
 
+
 msqla.ModelConverter.SQLA_TYPE_MAPPING[geoalchemy2.Geometry] = GeometryField
+msqla.ModelConverter.SQLA_TYPE_MAPPING[sqlalchemy.Numeric] = NumericField
+
 
 def make_annotation_flask_schema(model):
     model_name = model.__name__.capitalize() + "Schema"
