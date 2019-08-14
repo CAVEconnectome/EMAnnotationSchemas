66d59edb473d9d2db8f5713ad3ec24940f80d90b
Forrest Collman <forrest.collman@gmail.com>
2019-08-14 13:30:39 -0700

    added serialization fix

--- emannotationschemas/sqlalchemy_schemas.py
+++ emannotationschemas/sqlalchemy_schemas.py
@@ -1,6 +1,30 @@
 from flask_marshmallow import Marshmallow
+import marshmallow_sqlalchemy as msqla
+import geoalchemy2
+from shapely import geometry
+from geoalchemy2.shape import from_shape, to_shape
+
 
 ma = Marshmallow()
+
+class GeometryField(fields.Field):
+    """
+    Use shapely and geoalchemy2 to serialize / deserialize a point
+    Does make a big assumption about the data being spat back out as
+    JSON, but what the hey.
+    """
+
+    def _serialize(self, value, attr, obj):
+        if value is None:
+            return None
+        return geometry.mapping(to_shape(value))
+
+    def _deserialize(self, value, attr, data):
+        if value is None:
+            return None
+        return from_shape(geometry.shape(value))
+
+msqla.ModelConverter.SQLA_TYPE_MAPPING[geoalchemy2.Geometry] = GeometryField
 
 def make_annotation_flask_schema(model):
     model_name = model.__name__.capitalize() + "Schema"
