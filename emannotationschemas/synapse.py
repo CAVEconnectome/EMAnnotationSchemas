6576c59a16765686320d487448e1e34a256d2031
dlbrittain <dlbrittain@gmail.com>
2020-05-20 11:33:30 -0700

    synapse validation check

--- emannotationschemas/synapse.py
+++ emannotationschemas/synapse.py
@@ -1,6 +1,7 @@
 from emannotationschemas.base import BoundSpatialPoint, \
     SpatialPoint, \
     AnnotationSchema
+from marshmallow import ValidationError
 import marshmallow as mm
 
 
@@ -14,30 +15,30 @@ class SynapseSchema(AnnotationSchema):
     post_pt = mm.fields.Nested(BoundSpatialPoint, required=True,
                                description="presynaptic point",
                                order=2)
-    size = mm.fields.Float(description="size of synapse")
+    size = mm.fields.Float(description="size of synapse", missing=None)
+
+
+    @mm.validates_schema
+    def validate_type(self, data, **kwargs):
+        # check that the annotation type is present in the object as 'synapse'
+        if data["type"] != 'synapse':
+            raise mm.ValidationError("Type must be 'synapse'")
 
     @mm.post_load
-    def validate_type(self, item):
-        # check that the annotation type is present in the object as 'synapse'
-        assert item['type'] == 'synapse'
-
-        pre_id = item['pre_pt'].get('root_id', None)
-        post_id = item['post_pt'].get('root_id', None)
-
-        # if the root_id is present
+    def check_root_id(self, data, **kwargs):
+        pre_id = data['pre_pt'].get('root_id', None)
+        post_id = data['post_pt'].get('root_id', None)
+        # when the root_id is present
         # we should set the valid flag depending up on this rule
         # when the root_id is not present
         # (i.e. when posting new annotations with no root_id's in mind)
         # then the valid flag should be not present
         if pre_id is not None:
-            if (pre_id == post_id):
-                item['valid'] = False
-            else:
-                item['valid'] = True
+            data['valid'] = False if pre_id == post_id else True
         else:
-            item.pop('valid', None)
-        return item
-
+            data.pop('valid', None)
+        return data
+    
 
 class PlasticSynapse(SynapseSchema):
     plasticity = mm.fields.Int(required=True,
