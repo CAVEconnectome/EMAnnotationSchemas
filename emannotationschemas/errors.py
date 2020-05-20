00c188dc3d16a3cc721beccfb05f5829c0967123
dlbrittain <dlbrittain@gmail.com>
2020-05-20 11:30:25 -0700

    Moved custom excpetion to errors file

--- emannotationschemas/errors.py
+++ emannotationschemas/errors.py
@@ -4,3 +4,8 @@ class UnknownAnnotationTypeException(Exception):
 
 class InvalidTableMetaDataException(Exception):
     '''exception is raised when metadata for a table is not valid or is missing'''
+
+
+
+class InvalidSchemaField(Exception):
+    '''Exception raised if a schema can't be translated to a model'''
