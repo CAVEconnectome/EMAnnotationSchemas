74343029d23df7a4a6121a88cc5d8ccf6d11ec7d
Forrest Collman <forrest.collman@gmail.com>
2020-06-18 09:45:16 -0700

    removing MetaData

--- emannotationschemas/models.py
+++ emannotationschemas/models.py
@@ -53,20 +53,6 @@ class ModelStore:
 
 annotation_models = ModelStore()
 
-class Metadata(Base):
-    __tablename__ = 'annotation_table_metadata'
-    id = Column(Integer, primary_key=True)
-    schema_type = Column(String(100), nullable=False)
-    table_name = Column(String(100), nullable=False)
-    dataset_name = Column(String(100), nullable=False)
-    valid = Column(Boolean)
-    created = Column(DateTime, nullable=False)
-    deleted = Column(DateTime, nullable=True)
-    user_id = Column(String(255), nullable=False)
-    description = Column(Text, nullable=False)
-    reference_table = Column(String(100), nullable=True)
-    __table_args__ = (UniqueConstraint('table_name', 'dataset_name', name='_dataset_table_uc'),)
-
 def format_database_name(dataset: str, version: int = 1):
     return f"{dataset}_v{version}"
 
