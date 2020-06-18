55ef75ff7d1412d801332db235d08188fc50c565
Forrest Collman <forrest.collman@gmail.com>
2020-06-17 21:37:46 -0700

    added datasetname

--- emannotationschemas/models.py
+++ emannotationschemas/models.py
@@ -57,13 +57,14 @@ class Metadata(Base):
     id = Column(Integer, primary_key=True)
     schema_type = Column(String(100), nullable=False)
     table_name = Column(String(100), nullable=False)
+    dataset_name = Column(String(100), nullable=False)
     valid = Column(Boolean)
     created = Column(DateTime, nullable=False)
     deleted = Column(DateTime, nullable=True)
     user_id = Column(String(255), nullable=False)
     description = Column(Text, nullable=False)
     reference_table = Column(String(100), nullable=True)
-
+    
 
 def format_database_name(dataset: str, version: int = 1):
     return f"{dataset}_v{version}"
