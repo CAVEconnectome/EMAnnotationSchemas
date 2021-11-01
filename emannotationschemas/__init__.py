2530a9e78660fefbf4586dae668db23ab3fb3840
sdorkenw <sven.dorkenwald@googlemail.com>
2021-11-01 14:52:24 -0700

    Bump version: 3.3.0 → 3.4.0

--- emannotationschemas/__init__.py
+++ emannotationschemas/__init__.py
@@ -32,7 +32,7 @@ from emannotationschemas.schemas.proofreading import (
 from emannotationschemas.errors import UnknownAnnotationTypeException
 from emannotationschemas.flatten import create_flattened_schema
 
-__version__ = "3.3.0"
+__version__ = "3.4.0"
 
 type_mapping = {
     "synapse": SynapseSchema,
