1a8e96818d47391c3b6c3bb6165b59d2eac6589d
Derrick Brittain <dlbrittain@gmail.com>
2021-11-16 10:50:50 -0800

    test: check that non-ref table skips ref columns

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,15 +3,20 @@ import pytest
 from emannotationschemas.models import (
     Base,
     InvalidSchemaField,
+    make_annotation_model,
     make_annotation_model_from_schema,
     make_dataset_models,
 )
+
 
 def test_model_creation():
     metadata_dict = {"spinecall": {"reference_table": "synapse"}}
     model_dict = make_dataset_models(
         aligned_volume="test",
-        schemas_and_tables=[("synapse", "synapse"), ("postsynaptic_compartment", "spinecall")],
+        schemas_and_tables=[
+            ("synapse", "synapse"),
+            ("postsynaptic_compartment", "spinecall"),
+        ],
         metadata_dict=metadata_dict,
         include_contacts=True,
     )
@@ -22,11 +27,21 @@ def test_model_creation():
     contact_model = model_dict["contact"]
     assert contact_model.__name__ == "test__contact"
     assert issubclass(contact_model, Base)
-    
+
     ref_model = model_dict["spinecall"]
     assert ref_model.__name__ == "spinecall"
     assert issubclass(ref_model, Base)
-    # TODO better tests here
+
+
+def test_wrong_reference_schmea():
+    """Check that non-reference schema skips
+    reference schema columns during model creation when
+    optional metadata dict is passed with a reference"""
+    table_name = "bad_reference_table"
+    schema_type = "synapse"
+    table_metadata = {"reference_table": "anno_table"}
+    ref_anno_model = make_annotation_model(table_name, schema_type, table_metadata)
+    assert not hasattr(ref_anno_model, "target_id")
 
 
 def test_model_failure():
