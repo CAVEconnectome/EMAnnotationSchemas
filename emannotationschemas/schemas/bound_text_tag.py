a99534c079ce59438d9e49f3549c080e819a4992
sdorkenw <sven.dorkenwald@googlemail.com>
2022-01-30 17:09:23 -0600

    added new tag schemas

--- emannotationschemas/schemas/bound_text_tag.py
+++ emannotationschemas/schemas/bound_text_tag.py
@@ -7,3 +7,24 @@ class BoundTagAnnotation(AnnotationSchema):
         BoundSpatialPoint, required=True, description="Location associated with the tag"
     )
     tag = mm.fields.String(required=True, description="Arbitrary text tag")
+
+
+class Bound2TagAnnotation(BoundTagAnnotation):
+    pt = mm.fields.Nested(
+        BoundSpatialPoint, required=True, description="Location associated with the tag"
+    )
+    tag2 = mm.fields.String(required=True, description="Arbitrary text tag")
+
+
+class BoundTagAnnotationUser(BoundTagAnnotation):
+    user_id = fields.Int(
+        required=True,
+        description=f"User who created the tag.",
+    )
+
+
+class Bound2TagAnnotationUser(Bound2TagAnnotation):
+    user_id = fields.Int(
+        required=True,
+        description=f"User who created the tag.",
+    )
