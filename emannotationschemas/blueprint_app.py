ed42ffaec44e3794098ce346e00f00cc6b0a01b1
Forrest Collman <forrest.collman@gmail.com>
2022-02-20 20:48:28 -0800

    Bump version: 3.8.1 → 3.8.2

--- emannotationschemas/blueprint_app.py
+++ emannotationschemas/blueprint_app.py
@@ -6,7 +6,7 @@ from middle_auth_client import auth_required
 from emannotationschemas import get_schema, get_types
 from emannotationschemas.errors import UnknownAnnotationTypeException
 
-__version__ = "3.8.1"
+__version__ = "3.8.2"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
