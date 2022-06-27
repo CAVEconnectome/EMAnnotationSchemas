b0b2d3a0f98568f869229bb473b6512c5489da0e
Forrest Collman <forrest.collman@gmail.com>
2022-06-27 12:55:27 -0700

    Bump version: 5.0.1 → 5.0.2

--- emannotationschemas/app.py
+++ emannotationschemas/app.py
@@ -8,7 +8,7 @@ from emannotationschemas.config import configure_app
 from emannotationschemas.utils import get_instance_folder_path
 from emannotationschemas.views import views_bp
 
-__version__ = "5.0.1"
+__version__ = "5.0.2"
 
 
 def create_app(test_config=None):
