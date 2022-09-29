6af67afbf470c1f423a431ef18554c2f3df26d7f
Forrest Collman <forrest.collman@gmail.com>
2022-09-29 16:12:00 -0700

    Bump version: 5.1.1 → 5.1.2

--- emannotationschemas/views.py
+++ emannotationschemas/views.py
@@ -4,7 +4,7 @@ from flask import Blueprint, jsonify, render_template
 
 from emannotationschemas import get_flat_schema, get_schema, get_types
 
-__version__ = "5.1.1"
+__version__ = "5.1.2"
 
 
 views_bp = Blueprint("views", __name__, url_prefix="/schema/views")
