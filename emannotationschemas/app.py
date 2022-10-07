a8b52ae501f65995a91aef372fb1d4da5fb593ec
Derrick Brittain <dlbrittain@gmail.com>
2022-10-07 10:15:25 -0700

    Bump version: 5.1.4 → 5.1.5

--- emannotationschemas/app.py
+++ emannotationschemas/app.py
@@ -8,7 +8,7 @@ from emannotationschemas.config import configure_app
 from emannotationschemas.utils import get_instance_folder_path
 from emannotationschemas.views import views_bp
 
-__version__ = "5.1.4"
+__version__ = "5.1.5"
 
 
 def create_app(test_config=None):
--- emannotationschemas/blueprint_app.py
+++ emannotationschemas/blueprint_app.py
@@ -5,7 +5,7 @@ from marshmallow_jsonschema import JSONSchema
 from emannotationschemas import get_schema, get_types
 from emannotationschemas.errors import UnknownAnnotationTypeException
 
-__version__ = "5.1.4"
+__version__ = "5.1.5"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
