64bb2a69c607e5acaa6af83327c1a661fb9fe92a
Stephan Gerhard <git@unidesign.ch>
2022-10-27 17:54:01 +0200

    Remove submission time from braincircuits schema

--- emannotationschemas/schemas/braincircuits.py
+++ emannotationschemas/schemas/braincircuits.py
@@ -7,7 +7,6 @@ class BrainCircuitsBoundTagAnnotationUser(AnnotationSchema):
         BoundSpatialPoint, required=True, description="Location associated with the tag"
     )
     tag = mm.fields.String(required=True, description="Arbitrary text tag")
-    submission_time = mm.fields.DateTime(required=True, description="When the tag was submitted")
     user_id = mm.fields.String(
         required=True,
         description=f"User who created the tag.",
