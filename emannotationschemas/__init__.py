47f8ab5dfd506ae420ff76af6f09b771c033a3f1
Forrest Collman <forrest.collman@gmail.com>
2024-03-06 08:01:20 -0800

    Bump version: 5.10.4 → 5.11.0

--- emannotationschemas/__init__.py
+++ emannotationschemas/__init__.py
@@ -57,7 +57,7 @@ from emannotationschemas.schemas.bound_bool_tag import (
 from emannotationschemas.errors import UnknownAnnotationTypeException
 from emannotationschemas.flatten import create_flattened_schema
 
-__version__ = "5.10.4"
+__version__ = "5.11.0"
 
 type_mapping = {
     "synapse": SynapseSchema,
