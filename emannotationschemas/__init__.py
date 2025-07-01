c42147a679ce567ea9320fe1f2119623d36ba78b
Forrest Collman <forrest.collman@gmail.com>
2025-07-01 08:50:12 -0700

    Bump version: 5.18.0 → 5.19.0

--- emannotationschemas/__init__.py
+++ emannotationschemas/__init__.py
@@ -69,7 +69,7 @@ from emannotationschemas.schemas.reference_text_float import (
 from emannotationschemas.errors import UnknownAnnotationTypeException
 from emannotationschemas.flatten import create_flattened_schema
 
-__version__ = "5.18.0"
+__version__ = "5.19.0"
 
 type_mapping = {
     "synapse": SynapseSchema,
--- emannotationschemas/blueprint_app.py
+++ emannotationschemas/blueprint_app.py
@@ -5,7 +5,7 @@ from marshmallow_jsonschema import JSONSchema
 from emannotationschemas import get_schema, get_types
 from emannotationschemas.errors import UnknownAnnotationTypeException
 
-__version__ = "5.18.0"
+__version__ = "5.19.0"
 
 authorizations = {
     "apikey": {"type": "apiKey", "in": "query", "name": "middle_auth_token"}
