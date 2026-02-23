fa344514df05536954e6275befb4cb5c377ff880
Ben Pedigo <benjamindpedigo@gmail.com>
2026-02-23 12:32:59 -0800

    add test

---
+++ tests/test_simple_group.py
@@ -0,0 +1,25 @@
+from emannotationschemas.schemas.groups import SimpleGroup, SimpleGroupIndexed
+
+good_simple_group = {
+    "pt": {"position": [31, 32, 33]},
+    "group_id": 42,
+}
+
+good_simple_group_indexed = {
+    "pt": {"position": [10, 20, 30]},
+    "group_id": 123,
+}
+
+
+def test_simple_group_schema():
+    schema = SimpleGroup()
+    result = schema.load(good_simple_group)
+    assert result["group_id"] == 42
+    assert result["pt"]["position"] == [31, 32, 33]
+
+
+def test_simple_group_indexed_schema():
+    schema = SimpleGroupIndexed()
+    result = schema.load(good_simple_group_indexed)
+    assert result["group_id"] == 123
+    assert result["pt"]["position"] == [10, 20, 30]
