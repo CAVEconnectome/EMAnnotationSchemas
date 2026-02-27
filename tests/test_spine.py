03f0470e6c53da9c4945d31f9ced8f4e335115f6
Ben Pedigo <benjamindpedigo@gmail.com>
2026-02-27 11:23:00 -0800

    fix tests

--- tests/test_spine.py
+++ tests/test_spine.py
@@ -1,9 +1,7 @@
 from emannotationschemas.schemas.postsynaptic_compartment import SpineWithInfo
 
 good_spine_with_info = {
-    "position": [100, 200, 300],
-    "supervoxel_id": 50,
-    "root_id": 10,
+    "pt": {"position": [100, 200, 300], "supervoxel_id": 50, "root_id": 10},
     "volume": 1.5,
     "n_inputs": 3,
 }
@@ -12,9 +10,9 @@ good_spine_with_info = {
 def test_spine_with_info_schema():
     schema = SpineWithInfo()
     result = schema.load(good_spine_with_info)
-    assert result["position"] == [100, 200, 300]
-    assert result["supervoxel_id"] == 50
-    assert result["root_id"] == 10
+    assert result['pt']["position"] == [100, 200, 300]
+    assert result['pt']["supervoxel_id"] == 50
+    assert result['pt']["root_id"] == 10
     assert result["volume"] == 1.5
     assert result["n_inputs"] == 3
 
@@ -23,13 +21,11 @@ def test_spine_with_info_optional():
     schema = SpineWithInfo()
     result = schema.load(
         {
-            "position": [100, 200, 300],
-            "supervoxel_id": 50,
-            "root_id": 10,
+            "pt": {"position": [100, 200, 300], "supervoxel_id": 50, "root_id": 10},
         }
     )
-    assert result["position"] == [100, 200, 300]
-    assert result["supervoxel_id"] == 50
-    assert result["root_id"] == 10
+    assert result['pt']["position"] == [100, 200, 300]
+    assert result['pt']["supervoxel_id"] == 50
+    assert result['pt']["root_id"] == 10
     assert "volume" not in result
     assert "n_inputs" not in result
